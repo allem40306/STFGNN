0b78779f8d85bfebf3930591e45dba4e830d5b22
allem40306 <allem40306@gmail.com>
2024-05-07 15:59:45 +0800

    refactor: 修改儲存路徑

--- run.sh
+++ run.sh
@@ -1,22 +1,13 @@
-generate(){
-    python3 main_4n0_3layer_12T_res.py --config config/$1/individual_3layer_12T.json --save > $1.txt
-    python3 result.py --config config/$1/individual_3layer_12T.json
-    mv *$1* $2_$1
+grpah(){
+    cd data
+    python Temporal_Graph_gen.py --dataset $1 --lag 6 --period 24
+    cd ..
 }
 
 experiment(){
-    echo $1
-    mkdir $2_$1
-    generate $2 $1
-    ls $1
+    echo $1_$2
+    mkdir $1_$2
+    python3 main_4n0_3layer_12T_res.py --config config/$1/individual_3layer_12T.json --save  --folder $1_$2 --test > $1_$2/$1.txt
 }
 
-experiment r01 Youbike_2
-# experiment r02 Youbike_2
-# experiment r03 Youbike_2
-# experiment r04 Youbike_2
-# experiment r05 Youbike_2
-
-# cd data
-# python Temporal_Graph_gen.py --dataset Youbike --lag 6 --period 24
-# cd ..
+experiment Youbike_1 r01
